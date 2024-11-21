ec367513e968f3ab357e633d19bfa5678c1922a5
Alef Porto <falejoaoalef@gmail.com>
2024-11-21 15:39:10 -0300

    feat: implementar cadastro

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -1,4 +1,5 @@
 import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 import tatuadores from '../models/tatuadores.js';
 
 async function cadastrarTatuador(req, res){
@@ -18,6 +19,28 @@ async function cadastrarTatuador(req, res){
     res.status(201).json({ mensagem: 'Tatuador cadastrado com sucesso.' });
 }
 
+async function logarTatuador(req, res){
+    const { email, senha } = req.body;
+
+    if(!email || !senha)
+        return res.status(400).json({ mensagem: 'Preencha todos os campos obrigatórios.' });
+
+    const tatuador = tatuadores.find(tatuador => tatuador.email === email);
+
+    if(!tatuador)
+        return res.status(400).json({ mensagem: 'E-mail ou senha inválidos.' });
+
+    const senhaValida = await bcrypt.compare(senha, tatuador.senha);
+
+    if(!senhaValida)
+        return res.status(400).json({ mensagem: 'E-mail ou senha inválidos.' });
+
+    const token = jwt.sign({ id: tatuador.email }, 'secreta-chave', { expiresIn: '1h' });
+
+    res.status(200).json({ mensagem: 'Login realizado com sucesso.' });
+}
+
 export {
-    cadastrarTatuador
+    cadastrarTatuador,
+    logarTatuador
 };
--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -1,8 +1,9 @@
 import express from 'express';
-import { cadastrarTatuador } from '../controllers/authController.js';
+import { cadastrarTatuador, logarTatuador } from '../controllers/authController.js';
 
 const router = express.Router();
 
 router.post('/cadastro', cadastrarTatuador);
+router.post('/login', logarTatuador);
 
 export default router;
