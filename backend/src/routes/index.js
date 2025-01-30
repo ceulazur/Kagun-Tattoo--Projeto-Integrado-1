36e6c56ba7b6fd7a65dbde04f016589b40e7b3c4
Alef Porto <falejoaoalef@gmail.com>
2025-01-30 16:36:13 -0300

    feature: implementando CRUD de produtos

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -3,6 +3,7 @@ import authRoutes from './authRoutes.js';
 import tatuadorRoutes from './tatuadorRoutes.js';
 import clientesRoutes from './clienteRoutes.js';
 import sessaoRoutes from './sessaoRoutes.js';
+import produtoRoutes from './produtoRoutes.js';
 
 const router = express.Router();
 
@@ -11,5 +12,6 @@ router.use('/auth', authRoutes);
 router.use('/tatuadores', tatuadorRoutes);
 router.use('/clientes', clientesRoutes);
 router.use('/sessoes', sessaoRoutes);
+router.use('/produtos', produtoRoutes);
 
 export default router;
---
+++ backend/src/routes/produtoRoutes.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import ProdutoController from '../controllers/ProdutoController.js';
+import authMiddleware from '../middlewares/authMiddleware.js';
+import paginatorMiddleware from '../middlewares/paginatorMiddleware.js';
+
+const router = express.Router();
+
+router.post('/cadastrar', authMiddleware, ProdutoController.cadastrarProduto);
+router.get('/listar', authMiddleware, paginatorMiddleware, ProdutoController.listarProdutos);
+router.get('/:id', authMiddleware, ProdutoController.buscarProdutoPorId);
+router.put('/:id', authMiddleware, ProdutoController.atualizarProduto);
+router.delete('/:id', authMiddleware, ProdutoController.excluirProduto);
+
+export default router;
